82a12620e821d794ccffeda21bdfea3ee1aeddf0
Abdallah@moringa <abdallah.abdul@student.moringaschool.com>
2021-06-14 12:12:33 +0300

    add all the changes

--- src/test/java/DatabaseRule.java
+++ src/test/java/DatabaseRule.java
@@ -6,11 +6,9 @@ public class DatabaseRule extends ExternalResource {
 
     @Override
     public void before(){
-        //use the line below if you want to run locally replacing user with your database username and pass with password
-//        DB.sql2o=new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker_test", "wangui", "33234159");
-        // The line below runs app on heroku comment the line below if you want to run your app locally these are my
-        // credentials so they  will not work for you
-        DB.sql2o=new Sql2o("jdbc:postgresql://ec2-18-209-187-54.compute-1.amazonaws.com:5432/d7m5mfo9caefm6", "tlafeaczpyudbw", "cf9f6b81d98c18d5012a80842c01928b0331586444c6b9c08bc8504f67cbc678");
+
+        DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker", "moringa", "Access");
+
     }
 
     @Override
